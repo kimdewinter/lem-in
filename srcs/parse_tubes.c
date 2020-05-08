31be24a74d4e7d4dbc82545d00bbee4a03e0a04e
kimthulhu <kdewinter87@protonmail.com>
2020-05-08 16:19:15 +0200

    feat: remove unnecessary intermediate function

--- srcs/parse_tubes.c
+++ srcs/parse_tubes.c
@@ -6,16 +6,11 @@
 /*   By: lravier <lravier@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/06 15:39:25 by lravier       #+#    #+#                 */
-/*   Updated: 2020/05/08 15:55:06 by kim           ########   odam.nl         */
+/*   Updated: 2020/05/08 16:14:25 by kim           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem-in.h"
-
-static ssize_t		add_tube(t_room *room, char *key)
-{
-	return (add_neighbour(room, key));
-}
 
 static ssize_t		add_tubes(t_map *map, char **rooms)
 {
@@ -26,9 +21,9 @@ static ssize_t		add_tubes(t_map *map, char **rooms)
 	room2 = (t_room *)search_ht(map->rooms, rooms[1]);
 	if (room1 == NULL || room2 == NULL)
 		return (EXIT_FAILURE);
-	if (add_tube(room1, rooms[1]) == EXIT_FAILURE)
+	if (add_neighbour(room1, rooms[1]) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	if (add_tube(room2, rooms[0]) == EXIT_FAILURE)
+	if (add_neighbour(room2, rooms[0]) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
