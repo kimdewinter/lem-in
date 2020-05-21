7f4806a3873679bbc81e45cca5aec10afc7d02ed
kimthulhu <kdewinter87@protonmail.com>
2020-05-21 16:20:34 +0200

    feat: fns to create/copy/add to bitroutes

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: lravier <lravier@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/28 11:18:06 by lravier       #+#    #+#                 */
-/*   Updated: 2020/05/20 16:59:45 by kim           ########   odam.nl         */
+/*   Updated: 2020/05/21 16:19:29 by kim           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,14 @@ typedef struct		s_room
 	size_t			routes_len;
 	size_t			*this_i;//array of the current t_room's index-position in each of the t_routes
 	uint64_t		*bitroom;//TO DO: make this initialize to NULL
+	size_t			room_i;//TO DO: make this initialize to the order in which the rooms were input
 }					t_room;
 
 typedef struct		s_route
 {
 	struct s_room	**route;
 	size_t			len;
+	uint64_t		*bitroute;
 }					t_route;
 
 typedef struct		s_map
@@ -50,6 +52,7 @@ typedef struct		s_map
 	t_room			*end;
 	struct s_table	*rooms;
 	void			*routes;//type to be determined
+	size_t			bitfield_len;//TO DO: make this initialize (rooms_amount / BITFIELD_SIZE + 1)
 }					t_map;
 
 typedef struct	s_input_reader
@@ -80,9 +83,11 @@ size_t				is_room(char *line);
 size_t				is_antmount(char *line);
 unsigned long long	ft_atoi_ll(char *line, size_t *overflow);
 ssize_t				route_new(t_map *map);
-ssize_t				bite_room(t_room *room,
-						const size_t rooms_amount,
-						const size_t room_index);
+
+// BITFIELD-TOOLKIT:
+ssize_t				bite_room_new(t_room *room, const t_map *map);//takes a room and stores it's bitfield-form in room->bitroom
+inline void			bite_route_add_room(t_route *route, const t_room *room);//takes a bitfield-form route and flips on a room's bit in it(WARNING: no success/failure return)
+ssize_t				bite_route_new(t_route *route, const t_map *map);//takes the route->route array and stores it in bitfield-form in route->bitroute
 
 //the following are merely functions for debugging:
 void	debug(t_map *map);
--- srcs/biter.c
+++ srcs/biter.c
@@ -6,32 +6,25 @@
 /*   By: kim <kim@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/20 15:13:42 by kim           #+#    #+#                 */
-/*   Updated: 2020/05/21 14:13:51 by kim           ########   odam.nl         */
+/*   Updated: 2020/05/21 16:15:24 by kim           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem-in.h"
 
-ssize_t			bite_room(t_room *room,
-							const size_t rooms_amount,
-							const size_t room_index)
+static ssize_t	bite_alloc(BITFIELD_TYPE **dest, const size_t bitfield_len)
 {
 	size_t	i;
-	size_t	bitfield_len;
 
-	if (room != NULL && room->bitroom == NULL)
+	if (*dest == NULL)
 	{
-		bitfield_len = rooms_amount / BITFIELD_SIZE + 1;
-		room->bitroom = (BITFIELD_TYPE *)malloc(
-			sizeof(BITFIELD_TYPE) * bitfield_len);//allocates too much in case of exact size-match, e.g. rooms_amount is 64/128/etc
-		if (room->bitroom != NULL)
+		*dest = (BITFIELD_TYPE *)malloc(sizeof(BITFIELD_TYPE) * bitfield_len);
+		if (*dest != NULL)
 		{
 			i = 0;
 			while (i < bitfield_len)
 			{
-				room->bitroom[i] = (i == room_index / BITFIELD_SIZE) ?
-					(BITFIELD_TYPE)1 << 63 - room_index % BITFIELD_SIZE:
-					(BITFIELD_TYPE)0;
+				dest[i] = (BITFIELD_TYPE)0;
 				i++;
 			}
 			return (EXIT_SUCCESS);
@@ -39,3 +32,64 @@ ssize_t			bite_room(t_room *room,
 	}
 	return (EXIT_FAILURE);
 }
+
+ssize_t			bite_room_new(t_room *room, const t_map *map)
+{
+	size_t	i;
+
+	if (room != NULL && room->bitroom == NULL &&
+		alloc_bitfield(&(room->bitroom), map->bitfield_len) == EXIT_SUCCESS)
+	{
+		i = 0;
+		while (i < map->bitfield_len)
+		{
+			room->bitroom[i] = (i == room->room_i / BITFIELD_SIZE) ?
+				(BITFIELD_TYPE)1 << 63 - room->room_i % BITFIELD_SIZE:
+				(BITFIELD_TYPE)0;
+			i++;
+		}
+		return (EXIT_SUCCESS);
+	}
+	return (EXIT_FAILURE);
+}
+/*
+** takes a room and stores it's bitfield-form in room->bitroom
+*/
+
+inline void		bite_route_add_room(t_route *route, const t_room *room)
+{
+	if (route != NULL && route->bitroute != NULL &&
+		room != NULL && room->bitroom != NULL)
+	{
+		route->bitroute[room->room_i / BITFIELD_SIZE] |=
+			room->bitroom[room->room_i / BITFIELD_SIZE];
+	}
+}
+/*
+** takes a bitfield-form route and flips on a room's bit in it
+** WARNING: this is an inline void fn for performance reasons
+** (to increase odds of being inlined),
+** it will not return success or failure so use with care
+*/
+
+ssize_t			bite_route_new(t_route *route, const t_map *map)
+{
+	size_t	i;
+
+	if (route != NULL && route->route != NULL && route->bitroute == NULL &&
+		bite_alloc(&(route->bitroute), map->bitfield_len) == EXIT_SUCCESS)
+	{
+		i = 0;
+		while (i < route->len)
+		{
+			if (route->route[i] != NULL)
+				bite_route_add_room(route, route->route[i]);
+			i++;
+		}
+		return (EXIT_SUCCESS);
+	}
+	return (EXIT_FAILURE);
+}
+/*
+** takes the route->route array and stores it in bitfield-form in route->bitroute
+*/
