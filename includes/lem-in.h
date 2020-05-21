1e62d526ffd5c162c057b9a5cd5feb0a844b2b83
kimthulhu <kdewinter87@protonmail.com>
2020-05-21 16:47:51 +0200

    bug: forgotten semicolon in lem-in.h

--- includes/lem-in.h
+++ includes/lem-in.h
@@ -6,7 +6,7 @@
 /*   By: lravier <lravier@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/28 11:18:06 by lravier       #+#    #+#                 */
-/*   Updated: 2020/05/21 16:45:02 by kim           ########   odam.nl         */
+/*   Updated: 2020/05/21 16:47:29 by kim           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,7 +90,7 @@ inline void			bite_route_add_room(t_route *route, const t_room *room);//takes a 
 ssize_t				bite_route_convert(t_route *route, const t_map *map);//takes the route->route array and stores it in bitfield-form in route->bitroute
 ssize_t				bite_route_copy(t_route *dst,
 						const t_route *src,
-						const t_map *map)//copies one t_route's bitfield-form route into another's
+						const t_map *map);//copies one t_route's bitfield-form route into another's
 
 //the following are merely functions for debugging:
 void				debug(t_map *map);
