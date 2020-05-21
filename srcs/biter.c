ce19074318a37c0c529f53eab2497b6105b717e2
kimthulhu <kdewinter87@protonmail.com>
2020-05-21 14:25:33 +0200

    feat: unnecessary re-calculation in bite_room

--- srcs/biter.c
+++ srcs/biter.c
@@ -6,7 +6,7 @@
 /*   By: kim <kim@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/20 15:13:42 by kim           #+#    #+#                 */
-/*   Updated: 2020/05/20 17:00:31 by kim           ########   odam.nl         */
+/*   Updated: 2020/05/21 14:13:51 by kim           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,17 @@ ssize_t			bite_room(t_room *room,
 							const size_t room_index)
 {
 	size_t	i;
+	size_t	bitfield_len;
 
 	if (room != NULL && room->bitroom == NULL)
 	{
+		bitfield_len = rooms_amount / BITFIELD_SIZE + 1;
 		room->bitroom = (BITFIELD_TYPE *)malloc(
-			sizeof(BITFIELD_TYPE) * (rooms_amount / BITFIELD_SIZE + 1));//allocates too much in case of exact size-match, e.g. rooms_amount is 64/128/etc
+			sizeof(BITFIELD_TYPE) * bitfield_len);//allocates too much in case of exact size-match, e.g. rooms_amount is 64/128/etc
 		if (room->bitroom != NULL)
 		{
 			i = 0;
-			while (i < rooms_amount / BITFIELD_SIZE + 1)
+			while (i < bitfield_len)
 			{
 				room->bitroom[i] = (i == room_index / BITFIELD_SIZE) ?
 					(BITFIELD_TYPE)1 << 63 - room_index % BITFIELD_SIZE:
