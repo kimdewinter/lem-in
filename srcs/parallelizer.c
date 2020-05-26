d06753f41a3b0ee54b752aab4a6214480e135057
kimthulhu <kdewinter87@protonmail.com>
2020-05-26 16:35:18 +0200

    bug: fixes after review by Laure

--- srcs/parallelizer.c
+++ srcs/parallelizer.c
@@ -6,7 +6,7 @@
 /*   By: kim <kim@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/20 15:10:41 by kim           #+#    #+#                 */
-/*   Updated: 2020/05/26 15:53:07 by kim           ########   odam.nl         */
+/*   Updated: 2020/05/26 16:32:37 by kim           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ static ssize_t	combinatron_setup(t_map *map,
 		if (bite_alloc_noval(&(child->bitroutes), map) == EXIT_FAILURE)
 			return (EXIT_FAILURE);
 		if (bite_bitroute_merge(&(child->bitroutes), parent->bitroutes,
-			map->routes[child->i], map) == EXIT_FAILURE)
+			map->routes[parent->i]->bitroute, map) == EXIT_FAILURE)
 			return (EXIT_FAILURE);
 		if (copy_routes(parent->routes, child->routes, rtes_to_combi) ==
 			EXIT_FAILURE)
@@ -103,7 +103,7 @@ static ssize_t	combinatron_cleanup(t_map *map, t_combi *combi, ssize_t ret_val)
 	return (EXIT_FAILURE);
 }
 
-static ssize_t	combinatron_commit_combi(t_map *map, t_poscom *combi)
+static ssize_t	combinatron_commit_combi(t_map *map, const t_poscom *combi)
 {
 	t_combi	*new;
 
@@ -111,8 +111,9 @@ static ssize_t	combinatron_commit_combi(t_map *map, t_poscom *combi)
 		combi->bitroutes != NULL &&
 		map->valid_combis_last_i < map->valid_combis_len)
 	{
-		new = &(map->valid_combis[map->valid_combis_last_i]);
-		new = (t_combi *)malloc(sizeof(t_combi) * 1);
+		map->valid_combis[map->valid_combis_last_i] =
+			(t_combi *)malloc(sizeof(t_combi) * 1);
+		new = map->valid_combis[map->valid_combis_last_i];
 		if (new == NULL)
 			return (EXIT_FAILURE);
 		new->routes =
@@ -166,6 +167,7 @@ ssize_t			parallelize(t_map *map)
 
 	if (map != NULL)
 	{
+		num_to_combi = 0;//TO DO: function that calculates this
 		map->valid_combis_len = 0;//TO DO: function that calculates this
 		map->valid_combis = 
 			(t_combi **)malloc(sizeof(t_combi) * map->valid_combis_len);
@@ -178,17 +180,16 @@ ssize_t			parallelize(t_map *map)
 			map->valid_combis_last_i++;
 		}
 		map->valid_combis_last_i = 0;
-		num_to_combi = 1;
-		while (num_to_combi < map->num_routes)
+		while (num_to_combi > 0)
 		{
-			//if (combinatron(map) == EXIT_FAILURE)
-				//return (EXIT_FAILURE);
-			num_to_combi++;
+			if (combinatron(map, NULL, num_to_combi) == EXIT_FAILURE)
+				return (EXIT_FAILURE);
+			num_to_combi--;
 		}
 		return (EXIT_SUCCESS);
 	}
 	return (EXIT_FAILURE);
-}
+}//TO DO: make it never search for more parallels than there are ways in/out of start/end room(lowest)
 /*
 ** the combinatron function and it's children could be safer, but it'd cost performance
 ** should we obsessively check parameters like Kim usually does, or not?
