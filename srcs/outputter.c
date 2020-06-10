719d1dae77a3018f9232ce26d3d49b29c78b7798
LER1990 <photoravier@gmail.com>
2020-06-10 14:23:42 +0200

    feat: print input

--- srcs/outputter.c
+++ srcs/outputter.c
@@ -6,15 +6,30 @@
 /*   By: lravier <lravier@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/09 13:54:50 by kim           #+#    #+#                 */
-/*   Updated: 2020/06/10 12:59:31 by lravier       ########   odam.nl         */
+/*   Updated: 2020/06/10 14:23:04 by lravier       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem-in.h"
 
+void		print_input(t_input_reader *input)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < input->num_lines)
+	{
+		ft_printf("%s\n", input->lines[i]);
+		i++;
+	}
+	ft_printf("\n");
+}
+
 void		output_result(t_input_reader *input, t_map *map)
 {
-	if (input)
-		calculate_ants_per_path(map->antmount, &map->solution);
-	print_solution(map);
+	(void)input;
+	calculate_ants_per_path(map->antmount, &map->solution);
+	// print_solution(map);
+	// printf("OUTPUT:\n");
+	print_input(input);
 }
