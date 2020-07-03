8e686a3eaf4ae6fa70cb8c121052699a0bee9ae5
kimthulhu <kdewinter87@protonmail.com>
2020-07-03 16:01:24 +0200

    feat: frees input in main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,32 @@
 /*   By: kim <kim@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/06 14:06:51 by kim           #+#    #+#                 */
-/*   Updated: 2020/06/30 15:52:02 by kim           ########   odam.nl         */
+/*   Updated: 2020/07/03 15:59:18 by kim           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lemin.h"
+
+static void		delete_input(t_input_reader *input)
+{
+	size_t	i;
+
+	i = 0;
+	if (input != NULL && input->lines != NULL)
+	{
+		while (i < input->num_lines)
+		{
+			if (input->lines[i] != NULL)
+			{
+				free(input->lines[i]);
+				input->lines[i] = NULL;
+			}
+			i++;
+		}
+		free(input->lines);
+		input->lines = NULL;
+	}
+}
 
 static ssize_t	setup_map(t_map *map)
 {
@@ -44,9 +65,11 @@ int				main(void)
 		parallelize(&map) == EXIT_SUCCESS &&
 		output_result(&input, &map) == EXIT_SUCCESS)
 	{
+		delete_input(&input);
 		delete_map(&map);
 		return (EXIT_SUCCESS);
 	}
+	delete_input(&input);
 	delete_map(&map);
 	return (EXIT_FAILURE);
 }
