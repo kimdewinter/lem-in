b035902586c3c0c9e62b4dc8fa62ce1440183edd
kimthulhu <kdewinter87@protonmail.com>
2020-07-03 16:16:43 +0200

    Revert "feat: move delete_input to parse_input.c"
    This reverts commit 003d8e0910f8a204ec2dbdea1dcaa4c8e8d783a9.

--- srcs/parse_input.c
+++ srcs/parse_input.c
@@ -6,32 +6,11 @@
 /*   By: lravier <lravier@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/06 14:06:39 by kim           #+#    #+#                 */
-/*   Updated: 2020/07/03 16:09:11 by kim           ########   odam.nl         */
+/*   Updated: 2020/06/29 20:18:54 by kim           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lemin.h"
-
-static void		delete_input(t_input_reader *input)
-{
-	size_t	i;
-
-	i = 0;
-	if (input != NULL && input->lines != NULL)
-	{
-		while (i < input->num_lines)
-		{
-			if (input->lines[i] != NULL)
-			{
-				free(input->lines[i]);
-				input->lines[i] = NULL;
-			}
-			i++;
-		}
-		free(input->lines);
-		input->lines = NULL;
-	}
-}
 
 ssize_t	parse_input(t_map *map, t_input_reader *input)
 {
@@ -45,10 +24,8 @@ ssize_t	parse_input(t_map *map, t_input_reader *input)
 			parse_tubes(input, map, &i) == EXIT_SUCCESS &&
 			sanitize_input(map) == EXIT_SUCCESS)
 		{
-			delete_input(input);
 			return (EXIT_SUCCESS);
 		}
 	}
-	delete_input(input);
 	return (EXIT_FAILURE);
 }
