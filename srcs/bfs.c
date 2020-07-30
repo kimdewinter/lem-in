33778d274b65d72df385b3a875bf9a739c29b1b4
kimthulhu <kdewinter87@protonmail.com>
2020-07-30 13:19:06 +0200

    feat: finished bfs_shortest, small fix to grow_route

--- srcs/bfs.c
+++ srcs/bfs.c
@@ -6,7 +6,7 @@
 /*   By: lravier <lravier@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/29 13:37:28 by kim           #+#    #+#                 */
-/*   Updated: 2020/07/29 15:50:46 by kim           ########   odam.nl         */
+/*   Updated: 2020/07/30 13:18:45 by kim           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,8 @@ static ssize_t	grow_route(t_bfs_route *route,
 	{
 		if (i + 1 == room->neighbours_len)// one remaining, can use current t_bfs_route
 		{
-			route->next_to_add = room->neighbours[i];
+			if (room_in_bitfield(room->neighbours[i], vault->visited) == 0)
+				route->next_to_add = room->neighbours[i];
 		}
 		else if (room_in_bitfield(room->neighbours[i], vault->visited) == 0)
 		{
@@ -55,17 +56,22 @@ ssize_t			bfs_shortest(t_map *map)
 
 	vault.len = 0;
 	vault.used = 0;
+	vault.shortest = NULL;
 	if (bite_bitroute_copy(vault.visited, map->start->bitroom, map) ==
 		EXIT_FAILURE)
-		return (EXIT_FAILURE);
-
-	i = 0;
-	while (i < map->start->neighbours_len)
+		return (EXIT_FAILURE);//add start room to visited
+	if (branch_route(NULL, &vault, map->start->neighbours[i], map) ==
+		EXIT_FAILURE ||
+		add_room(vault.routes[vault.used - 1], map->start) == EXIT_FAILURE)
+		return (EXIT_FAILURE);// set up initial neighbours of start
+	while (vault.shortest == NULL && vault.used > 0)
 	{
-		if (branch_route(NULL, &vault, map->start->neighbours[i], map) ==
-			EXIT_FAILURE ||
-			add_room(vault.routes[vault.used - 1], map->start) == EXIT_FAILURE)
-			return (EXIT_FAILURE);
-		i++;
+		i = 0;
+		while (i < vault.len && vault.shortest == NULL)//possibility of two equally short routes being found, only one is kept
+		{
+			if (grow_route(vault.routes[i], &vault, map) == EXIT_FAILURE)
+				return (EXIT_FAILURE);
+			i++;
+		}
 	}
 }
