932b710cace660aec095864761d55083b8f275a2
LER1990 <photoravier@gmail.com>
2020-08-26 11:20:40 +0200

    add author file and error tests

--- srcs/route_finder.c
+++ srcs/route_finder.c
@@ -6,7 +6,7 @@
 /*   By: lravier <lravier@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/15 14:33:23 by kim           #+#    #+#                 */
-/*   Updated: 2020/08/26 10:47:32 by lravier       ########   odam.nl         */
+/*   Updated: 2020/08/26 11:15:17 by lravier       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,13 @@ static ssize_t	free_and_ret(t_best *candidate, ssize_t ret)
 	return (ret);
 }
 
+static ssize_t	handle_no_paths_found(t_best *candidate)
+{
+	if (candidate->combi == NULL)
+		return (ft_error("Error: no valid path found\n", EXIT_FAILURE));
+	return (EXIT_SUCCESS);
+}
+
 ssize_t			find_routes(t_map *map)
 {
 	t_best	candidate;
@@ -50,7 +57,7 @@ ssize_t			find_routes(t_map *map)
 		if (find_parallel_routes(&candidate, map) == EXIT_FAILURE)
 			return (EXIT_FAILURE);
 		if (candidate.used == 0)
-			return (EXIT_SUCCESS);
+			return (handle_no_paths_found(&candidate));
 		if (set_weights(map, 1, NULL) == EXIT_FAILURE)
 			free_and_ret(&candidate, EXIT_FAILURE);
 		blocks_found = remove_blockage(&candidate, map);
